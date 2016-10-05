df5cc91cf9338a075c5d12fd5c3b991c66efc8fe
renisplace <renisplace@hotmail.com>
2016-10-05 00:14:02 -0400

    Recursive solution to problem 15

--- code/Main.java
+++ code/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 	
 		int i, n;
-		n=10;	// number of problems
+		n=0;	// number of problems
 		Method m; 
 		Object r; 
 		
@@ -30,14 +30,14 @@ public class Main {
 		}
 		
 		//test
-		/*long startTime = System.currentTimeMillis();
+		long startTime = System.currentTimeMillis();
 		
-		System.out.println("Problem9  .. " + Problem9.calculate()); 
+		System.out.println("Problem101  .. " + Problem15.calculate()); 
 		//System.out.println("isPrime  .. " + SharedFunctions.isPrime(600851475143L));
 		
 		long endTime = System.currentTimeMillis();
 		long totalTime = endTime - startTime;
-		System.out.println(totalTime);*/
+		System.out.println(totalTime);
 		
 		
 	}
---
+++ code/Problem15.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.List;
+import Annotations.*;
+import java.lang.Math;
+
+@problemDeff(
+		number = 9,
+		dateSolved = "10/3/16",
+		deff = "Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.",
+		notes = ""
+	)
+public class Problem15 {
+	protected static long p;
+	public static long calculate(){
+		
+		//grid height/width
+		int N = 20; 
+		p= 1;
+		
+		// if a node is (i,j) the next node in the path can only be (i,j+1) or (i+1,j)
+		// the possible paths in the grid can be represented by a binary tree with root (1,1), first level (1,2) and (2,1) etc. .. all leafs are (N+1,N+1)
+		// the number of paths increases when there is a split in the grid tree, for example (2,1) branches off to (2,2) and (3,1) when N>2
+		numPaths(1,1,N+1);
+		
+		return p;
+	}
+	
+	public static void numPaths(int i, int j, int n){
+		//System.out.println("(" + i + "," + j + ")");
+		if (i==n && j==n){
+			return;
+		}
+		if (i<n && j==n){
+			//don't increase the number of paths, still on the same one
+			numPaths(i+1,j,n);
+		}
+		if (i==n && j<n){
+			//don't increase the number of paths, still on the same one
+			numPaths(i,j+1,n);
+		}
+		if (i<n && j<n){
+			//increase the number of paths, this is a split
+			p++;
+			
+			//keep looking for splits to the left 
+			numPaths(i+1,j,n);
+			
+			//keep looking for splits down
+			numPaths(i,j+1,n);
+		}
+	}
+		
+}
